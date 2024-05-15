3fdca8cd0ad8be51e07b6f66267a54acf767e1c4
malfoyz <the13mufasa@gmail.com>
2024-05-15 18:57:30 +0700

    Создал калькулятор

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+def main(input: str):
+    symbols = input.split()
+
+    if len(symbols) != 3:
+        raise Exception('Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)')
+
+    s1, sign, s2 = symbols
+
+    if float(s1) != int(float(s1)) or float(s2) != int(float(s2)):
+        raise Exception('Числа не целые')
+
+    s1, s2 = map(int, [s1, s2])
+
+    if s1 < 1 or s1 > 10 or s2 < 1 or s2 > 10:
+        raise Exception('Числа не могут быть меньше 1 и больше 10')
+
+    if sign == '+':
+        return int(s1) + int(s2)
+    elif sign == '-':
+        return int(s1) - int(s2)
+    elif sign == '*':
+        return int(s1) * int(s2)
+    elif sign == '/':
+        return int(s1) // int(s2)
+
+
+while True:
+    print(main(input()))
